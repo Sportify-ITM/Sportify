87a891d2f6fb999f643c1e24788bf075d07dd9ab
Kim Kangmin <rkdals0203@gmail.com>
2023-11-07 16:28:18 +0900

    20231107 layout: Successful login page done by Kangmin

--- app/src/main/java/com/example/sportify/MainActivity.kt
+++ app/src/main/java/com/example/sportify/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.sportify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 //hi from kangmin
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
