a94f79547834dca72a459b11d0fa097d97ea9a10
jeongmino <91644928+jeongmino@users.noreply.github.com>
2023-11-14 18:42:15 +0900

    20231114 main 브랜치 MainActivity 실행가능

--- app/src/main/java/com/example/sportify/MainActivity.kt
+++ app/src/main/java/com/example/sportify/MainActivity.kt
@@ -8,10 +8,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
         setContentView(R.layout.activity_login_page)
-=======
-        setContentView(R.layout.activity_login_success)
->>>>>>> ac2c0d1a1e45920c6534b65314ba3e96a42effbf
+//        setContentView(R.layout.activity_login_success)
     }
 }
