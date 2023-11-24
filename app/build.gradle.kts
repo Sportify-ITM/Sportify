aa5e619fc6bc2f92537c3d994b387a3665c7c0d4
chanwoo24 <cksdn6110@naver.com>
2023-11-24 14:56:54 +0900

    20231124: GoogleMap CheckPoint 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -51,4 +54,11 @@ dependencies {
     // Google Map
     implementation("com.google.android.gms:play-services-maps:18.2.0") // Google Play services SDK version
     implementation("com.google.android.gms:play-services-location:21.0.0")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Realtime Database library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-database-ktx")
 }
--- app/src/main/java/com/example/sportify/MapActivity.kt
+++ app/src/main/java/com/example/sportify/MapActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.sportify.databinding.ActivityMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,6 +12,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -20,10 +22,13 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     var currentLat: Double = 0.0
     var currentLon: Double = 0.0
 
+    private val firestore = FirebaseFirestore.getInstance() // Firebase
+    private val locationsCollection = firestore.collection("locations") // Firebase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMapBinding.inflate(layoutInflater)
+        binding =    ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         currentLat = intent.getDoubleExtra("currentLat", 0.0)
@@ -39,10 +44,18 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.btnCheckHere.setOnClickListener() {
             mMap?.let{
                 val intent = Intent()
+                val location = it.cameraPosition.target // Firebase
                 intent.putExtra("latitude", it.cameraPosition.target.latitude)
                 intent.putExtra("longitude", it.cameraPosition.target.longitude)
-                setResult(Activity.RESULT_OK, intent)
-                finish()
+
+                // Save the location to Firestore
+                saveLocationToFirestore(location.latitude, location.longitude) // Firebase
+                saveLocationToFirestore(37.52990242301772, 126.96466236081207) // 용산역
+                saveLocationToFirestore(37.4670, 126.9491) // SNU
+                saveLocationToFirestore(37.5894, 127.0325) // Korea University
+
+                 setResult(Activity.RESULT_OK, intent)
+                 finish()
             }
         }
         binding.fabCurrentLocation.setOnClickListener {
@@ -52,7 +65,45 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
             mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude!!, longitude!!), 16f))
             setMarker()
+
+            // Retrieve and display locations from Firestore
+            retrieveLocationsFromFirestore()
         }
+    }
+
+    // Function to save location to Firestore
+    private fun saveLocationToFirestore(latitude: Double, longitude: Double) {
+        val locationData = hashMapOf(
+            "latitude" to latitude,
+            "longitude" to longitude
+        )
+
+        locationsCollection.add(locationData)
+            .addOnSuccessListener { documentReference ->
+                // Handle success if needed
+            }
+            .addOnFailureListener { e ->
+                // Handle failure if needed
+            }
+    }
+
+    // Function to retrieve locations from Firestore and display markers on the map
+    private fun retrieveLocationsFromFirestore() {
+        locationsCollection.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val latitude = document.getDouble("latitude")
+                    val longitude = document.getDouble("longitude")
+
+                    if (latitude != null && longitude != null) {
+                        val location = LatLng(latitude, longitude)
+                        mMap?.addMarker(MarkerOptions().position(location).title("Marker Location"))
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                // Handle failure if needed
+            }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -80,6 +131,21 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                     marker.position = it.cameraPosition.target
                 }
             }
+
+            it.setOnMarkerClickListener { clickedMarker ->
+                // 마커를 클릭했을 때의 동작
+                val markerPosition = clickedMarker.position
+                val latitude = markerPosition.latitude
+                val longitude = markerPosition.longitude
+
+                // 토스트 메시지로 위도와 경도 보여주기
+                Toast.makeText(this@MapActivity, "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_SHORT).show()
+
+                // true를 반환하면 마커의 기본 동작을 수행하지 않습니다.
+                true
+            }
+
+
         }
     }
 }
