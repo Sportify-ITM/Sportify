760fbf65a382af8eeb23c94474db1f439ec07484
Kim Kangmin <rkdals0203@gmail.com>
2023-12-15 20:12:34 +0900

    20231215 conflict resolve

--- app/src/main/java/com/example/sportify/CalendarFragment.kt
+++ app/src/main/java/com/example/sportify/CalendarFragment.kt
@@ -4,10 +4,10 @@ import MatchTeamItem
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sportify.adapter.CalenderCardAdpater
@@ -25,7 +25,8 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 class CalenderFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
