0f9a3b0285df46276274a8f4d9308d8af4bb27a1
jeongmino <91644928+jeongmino@users.noreply.github.com>
2023-12-17 07:08:54 +0900

    20231216 MatchDetail complete

---
+++ app/src/main/java/com/example/sportify/adapter/MatchDetailAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.sportify.adapter
+
+import MatchTeamItem
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sportify.R
+
+data class MatchData(
+    val name: String,
+    val homeValue: String,
+    val awayValue: String
+)
+
+class MatchDetailAdapter(private val matchDataList: List<MatchData>) : RecyclerView.Adapter<MatchDetailAdapter.MatchDetailViewHolder>() {
+
+    class MatchDetailViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var listName: TextView = view.findViewById(R.id.listName)
+        var home: TextView = view.findViewById(R.id.home)
+        var away: TextView = view.findViewById(R.id.away)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchDetailViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.match_detail_items, parent, false)
+        return MatchDetailViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MatchDetailViewHolder, position: Int) {
+        val matchData = matchDataList[position]
+        holder.listName.text = matchData.name
+        holder.home.text = matchData.homeValue
+        holder.away.text = matchData.awayValue
+    }
+
+    override fun getItemCount() = matchDataList.size
+}
